a30f10098bcf1f2131e297c628853e3db4f762db
Evgeniy Ryshkov <eryshkov@gmail.com>
2019-01-21 13:26:13 +0300

    concrete factories are added

---
+++ Abstract Factory/Model/ConcreteFactory/BedroomFactory.swift
@@ -0,0 +1,32 @@
+//
+//  BedroomFactory.swift
+//  Abstract Factory
+//
+//  Created by Evgeniy Ryshkov on 21/01/2019.
+//  Copyright © 2019 Evgeniy Ryshkov. All rights reserved.
+//
+
+import Foundation
+
+class BedroomFactory: AbstractFactory {
+    
+    func createChair() -> Chair {
+        let result = BedroomChair()
+        print("\(result.type) создан")
+        return result
+    }
+    
+    func createSofa() -> Sofa {
+        let result = BedroomSofa()
+        print("\(result.type) создан")
+        return result
+    }
+    
+    func createTable() -> Table {
+        let result = BedroomTable()
+        print("\(result.type) создан")
+        return result
+    }
+    
+    
+}
---
+++ Abstract Factory/Model/ConcreteFactory/KitchenFactory.swift
@@ -0,0 +1,31 @@
+//
+//  KitchenFactory.swift
+//  Abstract Factory
+//
+//  Created by Evgeniy Ryshkov on 21/01/2019.
+//  Copyright © 2019 Evgeniy Ryshkov. All rights reserved.
+//
+
+import Foundation
+
+class KitchenFactory: AbstractFactory {
+    func createChair() -> Chair {
+        let result = KitchenChair()
+        print("\(result.type) создан")
+        return result
+    }
+    
+    func createSofa() -> Sofa {
+        let result = KitchenSofa()
+        print("\(result.type) создан")
+        return result
+    }
+    
+    func createTable() -> Table {
+        let result = KitchenTable()
+        print("\(result.type) создан")
+        return result
+    }
+    
+    
+}
