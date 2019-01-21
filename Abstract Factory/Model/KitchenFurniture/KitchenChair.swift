498ad6a73d5203353c9be921d111dabe71342ab5
Evgeniy Ryshkov <eryshkov@gmail.com>
2019-01-21 13:13:19 +0300

    kitchen classes are added

---
+++ Abstract Factory/Model/KitchenFurniture/KitchenChair.swift
@@ -0,0 +1,17 @@
+//
+//  KitchenChair.swift
+//  Abstract Factory
+//
+//  Created by Evgeniy Ryshkov on 21/01/2019.
+//  Copyright © 2019 Evgeniy Ryshkov. All rights reserved.
+//
+
+import Foundation
+
+class KitchenChair: Chair {
+    var name: String = "Стул"
+    
+    var type: String = "Стул для кухни"
+    
+    
+}
---
+++ Abstract Factory/Model/KitchenFurniture/KitchenTable.swift
@@ -0,0 +1,17 @@
+//
+//  KitchenTable.swift
+//  Abstract Factory
+//
+//  Created by Evgeniy Ryshkov on 21/01/2019.
+//  Copyright © 2019 Evgeniy Ryshkov. All rights reserved.
+//
+
+import Foundation
+
+class KitchenTable: Table {
+    var name: String = "Стол"
+    
+    var type: String = "Стол для кухни"
+    
+    
+}
