fe4568b26c92d719a288247cb784440a1ac8e869
Evgeniy Ryshkov <eryshkov@gmail.com>
2019-01-21 13:08:50 +0300

    bedroom classes are added

---
+++ Abstract Factory/Model/BedroomFurniture/BedroomChair.swift
@@ -0,0 +1,17 @@
+//
+//  BedroomChair.swift
+//  Abstract Factory
+//
+//  Created by Evgeniy Ryshkov on 21/01/2019.
+//  Copyright © 2019 Evgeniy Ryshkov. All rights reserved.
+//
+
+import Foundation
+
+class BedroomChair: Chair {
+    var name: String = "Стул"
+    
+    var type: String = "Стул для спальни"
+    
+    
+}
---
+++ Abstract Factory/Model/BedroomFurniture/BedroomSofa.swift
@@ -0,0 +1,17 @@
+//
+//  BedroomSofa.swift
+//  Abstract Factory
+//
+//  Created by Evgeniy Ryshkov on 21/01/2019.
+//  Copyright © 2019 Evgeniy Ryshkov. All rights reserved.
+//
+
+import Foundation
+
+class BedroomSofa: Sofa {
+    var name: String = "Диван"
+    
+    var type: String = "Диван для спальни"
+    
+    
+}
